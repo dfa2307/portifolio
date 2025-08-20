8b6872b20a3b654a438b69e4a7e505b35530e5f1
dfa2307 <dan.franciscoo@gmail.com>
2025-08-19 22:04:15 -0300

    modified:   app.js 	new file:   img/icons8-github-250.png 	new file:   img/icons8-linkedin-240.png 	new file:   img/icons8-outlook-240.png 	modified:   index.html 	modified:   src/output.css 	modified:   src/style.css

--- app.js
+++ app.js
@@ -24,3 +24,43 @@ btnNav.addEventListener("click", ()=>{
   btnNav.classList.toggle("ativo")
   menuNav.classList.toggle("menuNav")
 })
+
+// CRIAÇÃO DE UM OBSERVADOR, PARA SABER SE O ELEMNETO ESTA SENDO VISTO NA TELA OU NÃO, CASO ELE ESTEJA SENDO VISTO NA TELA ELE IRÁ ADICIONAR A CLASSE SHOW, QUE FAZ COM QUE O ELEMENTO APAREÇA
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => { //FOREACH USADO PARA PEGAR CADA ELEMENTO, JÁ QUE A INSTÂNCIA QUE CRIAMOS NO IntersectionObserver CRIA UM ARRAY COM TODOS OS ELEMENTOS QUE TENHAM AS CLASSES INVISIBLE QUE DECLARAMOS ABAIXO
+    if(entry.isIntersecting){
+      entry.target.classList.add("showLE")
+    }else{
+      entry.target.classList.remove("showLE")
+    }
+  })
+})
+
+const elements = document.querySelectorAll(".invisibleLE") //AQUI PEGUEI DO DOM TODOS OS ELEMENTOS COM A CLASSE ".invisibleLE" QUE SÃO TODOS OS ELEMENTOS DO LADO ESQUERDO DA DIV
+
+elements.forEach((element) => { //AQUI COLOQUEI ELE EM UM FOREACH PARA PEGAR CADA UM DOS ELEMENTOS EM PARTICULAR E APLICAR O MÉTODO OBSERVE
+  myObserver.observe(element)
+})
+
+
+
+
+
+
+
+
+const myObserverLD = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("showLD")
+    }else{
+      entry.target.classList.remove("showLD")
+    }
+  })
+})
+
+const elementsLD = document.querySelectorAll(".invisibleLD")
+
+elementsLD.forEach((elementLD) =>{
+  myObserverLD.observe(elementLD)
+})
